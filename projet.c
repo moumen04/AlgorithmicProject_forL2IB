69b8297b1ffc53abcdda5cb0e4b0b2b92bce568c
WassimDorbane <121866043+WassimDorbane@users.noreply.github.com>
2023-12-27 18:30:46 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -12,17 +12,16 @@ typedef struct liste
 
 //ALLOCATION
 liste *add_liste(){
-    liste *p=(liste*)malloc(sizeof(liste));}
+    return (liste*)malloc(sizeof(liste));}
 
 //INITIALISATION
 void initialiser_liste(liste **ptete,liste **pqueue){
     ptete=NULL;
     pqueue=NULL;}
 
-//RAMPLICAGE
-
-liste * ramplire_liste(liste **ptete,liste **pqueue){
-    int x;
+//REMPLICAGE
+liste * remplire_liste(liste **ptete,liste **pqueue){
+    int x;*ptete=add_liste();*pqueue=add_liste();
     printf("entrer le 1er entier: ");
     scanf("%d",&x);
     (*ptete)->info=x;
@@ -33,34 +32,82 @@ liste * ramplire_liste(liste **ptete,liste **pqueue){
     (*pqueue)->preced=*ptete;
     (*pqueue)->suivant=NULL;
     (*ptete)->suivant=*pqueue;
-    int i=3; char c;
+    int i=3; char c='y';
     do
-    {   printf("entrer 'y'si tu veut continuer");
-        scanf("%c",&c);
+    {   printf("entrer 'Y'si tu veut continuer: ");
+        scanf("%s",&c);
         switch (c)
         {
-        case 'y':
+        case 'Y':
             liste *temp=add_liste();
-            printf("entrer le %deme entier:");
-            scanf("%d",&temp->info);
+            printf("entrer le %deme entier:",i);i++;int x;
+            scanf("%d",&x);
+            temp->info=x;
             temp->preced=*pqueue;
             temp->suivant=NULL;
+            (*pqueue)->suivant=temp;
             *pqueue=temp;
             break;
         
-        default:printf("donc vous voullez quiter");
+        default:printf("*****FIN DE REMPLICAGE*****");
             break;
         }
         
     
-    } while (c=='y');
-    
+    } while (c=='Y');
 
+
+}
+
+//AFFICHAGE 
+void afficher_liste(liste **ptete){
+    liste *temp=add_liste();
+    temp=*ptete;printf("\n");
+    while (temp!=NULL)
+    {printf("| %d |-->",temp->info);
+     temp=temp->suivant;
+    }
+    printf("NULL\n*****FIN D'AFFICHAGE*****");}
+
+
+
+//TRIE PAR CELECTION
+void trier_liste(liste **ptete){
+    liste *tetetemp=add_liste(),*test=add_liste();
+    int min;
+    tetetemp=*ptete;
+    while (tetetemp!=NULL)
+    {int min=tetetemp->info;
+     test=tetetemp->suivant;
+     while (test!=NULL)
+     {if (test->info<min)
+     {int perm=test->info;
+     test->info=tetetemp->info;
+     tetetemp->info=perm;
+     }
+     test=test->suivant;
+     }
+     tetetemp=tetetemp->suivant;
+    }
     
 }
-int main (){
 
-printf("*****************************");
+
+
+
+
+
+int main (){
+    liste *ptete=NULL;liste *pqueue=NULL;
+    initialiser_liste(&ptete,&pqueue);
+    remplire_liste(&ptete,&pqueue);
+    afficher_liste(&ptete);
+    printf("\nEND OF PROGRAMME :)");
+
+
+
+    trier_liste(&ptete);
+    afficher_liste(&ptete);
 
 
 }
